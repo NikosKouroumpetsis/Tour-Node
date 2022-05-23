5ff2c49ad3b8061de22e7ca6ed735411b4ccee64
NikosKouroumpetsis <kourou67s@gmail.com>
2022-05-23 21:11:24 +0300

    patch and delete requests

--- app.js
+++ app.js
@@ -69,6 +69,30 @@ app.post('/api/v1/tours', (req, res) => {
   // res.send('Done');
 });
 
+app.patch('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
+  }
+
+  res.status(200).json({
+    status: 'Success',
+    data: {
+      tour: '<Updated Tour>',
+    },
+  });
+});
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
+  }
+
+  res.status(204).json({
+    status: 'Success',
+    data: null,
+  });
+});
+
 const port = 3000;
 
 app.listen(port, () => {
