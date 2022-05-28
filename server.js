8b74303da80d68af5630900486262028216920a0
NikosKouroumpetsis <kourou67s@gmail.com>
2022-05-28 22:39:31 +0300

    create and testTour Model

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const app = require('./app');
 
-dotenv.config({ path: '../vars/config.env' });
+dotenv.config({ path: './config.env' });
 
 const pwd = process.env.DATABASE_PASSWORD;
 const db = process.env.DATABASE;
@@ -29,12 +30,40 @@ mongoose
 //   console.log('DB connection successful!');
 // });
 
-const app = require('./app');
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
 
-const port = process.env.PORT || 3000;
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Park Camper',
+  price: 997,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log(`Error`, err);
+  });
 
 // console.log(process.env);
-
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server listening at ${port}...`);
 });
