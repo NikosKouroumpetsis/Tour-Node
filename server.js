48ed428d75610f107c951b38599835a7eae5a2b0
NikosKouroumpetsis <kourou67s@gmail.com>
2022-05-28 17:39:16 +0300

    connect app with mongo atlas

--- server.js
+++ server.js
@@ -1,5 +1,33 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
 dotenv.config({ path: 'config.env' });
+
+const pwd = process.env.DATABASE_PASSWORD;
+const db = process.env.DATABASE;
+const DB = db.replace('<PASSWORD>', pwd);
+
+// Connect to atlas
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('DB connection successful!');
+  });
+
+// Connect to localDB
+
+// mongoose
+// .connect(process.env.DATABASE_LOCAL, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// })
+// .then(() => {
+//   console.log('DB connection successful!');
+// });
 
 const app = require('./app');
 
