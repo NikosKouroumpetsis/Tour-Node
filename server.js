ab81e0ce2aa4eabf3cb075fba6c1945396a81848
NikosKouroumpetsis <kourou67s@gmail.com>
2022-06-05 23:25:04 +0300

    user Schema

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
+process.on('uncaughtException', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLER REJECTION!! Shutting down...');
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
@@ -39,6 +45,7 @@ app.listen(port, () => {
 process.on('unhandledRejection', (err) => {
   console.log(err.name, err.message);
   console.log('UNHANDLER REJECTION!! Shutting down...');
+  // eslint-disable-next-line no-undef
   server.close(() => {
     process.exit(1);
   });
